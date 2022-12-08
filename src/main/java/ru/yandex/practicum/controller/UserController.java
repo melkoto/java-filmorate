c25c9bc99924c0b8d21630bd9ecce8a9ec106c0d
Movsar Makhmutov <m.movsar@gmail.com>
2022-12-09 02:45:03 +0300

    added logs to UserController

--- src/main/java/ru/yandex/practicum/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.exceptions.AlreadyExistsException;
 import ru.yandex.practicum.exceptions.DoesntExistException;
@@ -11,6 +12,7 @@ import ru.yandex.practicum.model.User;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("users")
 public class UserController {
@@ -26,14 +28,17 @@ public class UserController {
         int id = user.getId();
 
         if (user.getLogin().split(" ").length > 1) {
+            log.error("Login cant have empty space");
             throw new InvalidLoginException("Login cant have empty space");
         }
 
         if (users.containsKey(id)) {
+            log.error("User with id = {} already exists.", id);
             throw new AlreadyExistsException("User with id = " + id + " already exists.");
         }
 
         users.put(id, user);
+        log.info("Added {} to users data", user.getName());
         return user;
     }
 
@@ -41,14 +46,17 @@ public class UserController {
     public User updateUser(@Valid @RequestBody User user, @RequestParam(value = "id", required = true) int id) {
 
         if (user.getLogin().split(" ").length > 1) {
+            log.error("Login cant have empty space");
             throw new InvalidLoginException("Login cant have empty space");
         }
 
         if (!users.containsKey(id)) {
+            log.error("User with id = {} does not exist.", user.getId());
             throw new DoesntExistException("User with id = " + id + " does not exist.");
         }
 
         users.put(id, user);
+        log.info("User with id = {} is updated", user.getId());
         return user;
     }
 }
