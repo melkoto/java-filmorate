ee45c41ab9af6d58c51c8f827f734244b4c45a6a
Movsar Makhmutov <m.movsar@gmail.com>
2023-01-04 21:24:57 +0500

    # Sprint 10.2

--- src/test/java/ru/yandex/practicum/controllers/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/controllers/UserControllerTest.java
@@ -3,49 +3,59 @@ package ru.yandex.practicum.controllers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.models.User;
+import ru.yandex.practicum.service.user.UserService;
+import ru.yandex.practicum.storage.film.InMemoryFilmStorage;
+import ru.yandex.practicum.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
 import java.time.Month;
 
+@SpringBootTest
 class UserControllerTest {
-    UserController userController;
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    InMemoryUserStorage userStorage;
+
     User user1;
     User user2;
 
     @BeforeEach
     void setUp() {
-        // userController = new UserController(userStorage, userService);
         user1 = new User();
-        user1.setId(1);
+//        user1.setId(1);
         user1.setEmail("test@test.com");
         user1.setLogin("test");
         user1.setName("Test");
         user1.setBirthday(LocalDate.of(1989, Month.AUGUST, 31));
 
         user2 = new User();
-        user2.setId(1);
-        user2.setEmail("test-another@test.com");
-        user2.setLogin("test-another");
-        user2.setName("Another");
-        user2.setBirthday(LocalDate.of(1994, Month.JANUARY, 29));
+//        user2.setId(1);
+        user2.setEmail("test@test.com");
+        user2.setLogin("test");
+        user2.setName("Test");
+        user2.setBirthday(LocalDate.of(1989, Month.AUGUST, 31));
     }
 
     @Test
     void getUsers() {
-        Assertions.assertEquals(0, userController.getUsers().size());
+        Assertions.assertEquals(3, userStorage.getUsers().size());
     }
 
     @Test
     void createUser() {
-        userController.createUser(user1);
-        Assertions.assertEquals(1, userController.getUsers().size());
+        userStorage.addUser(user1);
+        Assertions.assertEquals(3, userStorage.getUsers().size());
     }
 
     @Test
     void updateUser() {
-        userController.createUser(user1);
-        userController.updateUser(user2);
-        Assertions.assertEquals(userController.getUsers().get(0), user2);
+        userStorage.addUser(user1);
+        userStorage.addUser(user2);
+        Assertions.assertEquals(2, userStorage.getUsers().size());
     }
 }
